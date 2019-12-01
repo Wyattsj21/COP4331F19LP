3ca5a26d423a696542ed1fd3dd373a6391fdf7ae
DatN132 <54608515+DatN132@users.noreply.github.com>
2019-12-01 18:44:40 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+    <script>window.location.replace("/Deflate");</script>
+ <head>
+  <title>POOS Small Project</title>
+  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <style>
+      body{
+            background : url("background3.jpg") no-repeat center center fixed;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+        
+        h1.centered{
+            text-align: center;
+        }
+        .open-button {
+      border: none;
+      cursor: pointer;
+      opacity: 0.8;
+      position: fixed;
+      bottom: 20px;
+      right: 200px;
+      left:200px;
+      max-width: 100%;
+      height: auto;
+    }
+    div {
+            resize: none;
+            overflow: auto;
+        }
+    img {
+            max-width: 100%;
+            height: auto;
+        }
+  </style>
+ </head>
+
+ <body>
+    <div style="background:transparent !important" class="jumbotron">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-6">
+                    <div class="card text-dark bg-light shadow">
+                        <div class="card-body">
+                            <h1 class="card-title text-dark text-center">Contact Manager</h1>
+                            <a class="shadow btn btn-primary btn-block" href="login.php" roll="button">Go To War</a>
+                            <a class="shadow btn btn-primary btn-block" href="sign_up.php" roll="button">Enlist</a>
+                            <a class="shadow btn btn-primary btn-block" href="Deflate" roll="button">Deflate</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+   
+ </body>
+ <footer>
+      <!--<center><div class ="open-button"><a href="http://getamped2.cyberstep.com/" target="_blank"><img src="shooting.gif"></a></div></center>-->
+ </footer>
+</html>

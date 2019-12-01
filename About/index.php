c22c9494f00572e4c3ff85670d9b2e0579d11743
DatN132 <54608515+DatN132@users.noreply.github.com>
2019-12-01 18:44:57 -0500

    Add files via upload

--- About/index.php
+++ About/index.php
@@ -1,66 +1,137 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-    <script>window.location.replace("/Deflate");</script>
- <head>
-  <title>POOS Small Project</title>
-  
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <style>
-      body{
-            background : url("background3.jpg") no-repeat center center fixed;
-            background-repeat: no-repeat;
-            background-size: cover;
-        }
-        
-        h1.centered{
-            text-align: center;
-        }
-        .open-button {
-      border: none;
-      cursor: pointer;
-      opacity: 0.8;
-      position: fixed;
-      bottom: 20px;
-      right: 200px;
-      left:200px;
-      max-width: 100%;
-      height: auto;
-    }
-    div {
-            resize: none;
-            overflow: auto;
-        }
-    img {
-            max-width: 100%;
-            height: auto;
-        }
-  </style>
- </head>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
- <body>
-    <div style="background:transparent !important" class="jumbotron">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<style>
+   body {
+  background: #fafafa;
+  color: #333333;
+  margin-top: 5rem;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  color: #444444;
+}
+
+.bg-steel {
+  background-color: #5f788a;
+}
+
+.site-header .navbar-nav .nav-link {
+  color: #cbd5db;
+}
+
+.site-header .navbar-nav .nav-link:hover {
+  color: #ffffff;
+}
+
+.site-header .navbar-nav .nav-link.active {
+  font-weight: 500;
+}
+
+.content-section {
+  background: pink;
+  padding: 10px 20px;
+  border: 1px solid #dddddd;
+  border-radius: 3px;
+  margin-bottom: 20px;
+}
+
+.article-title {
+  color: #444444;
+}
+
+a.article-title:hover {
+  color: #428bca;
+  text-decoration: none;
+}
+
+.article-content {
+  white-space: pre-line;
+}
+
+.article-img {
+  height: 65px;
+  width: 65px;
+  margin-right: 16px;
+}
+
+.article-metadata {
+  padding-bottom: 1px;
+  margin-bottom: 4px;
+  border-bottom: 1px solid #e3e3e3
+}
+
+.article-metadata a:hover {
+  color: #333;
+  text-decoration: none;
+}
+
+.article-svg {
+  width: 25px;
+  height: 25px;
+  vertical-align: middle;
+}
+
+</style>
+
+        <title>Deflate</title>
+
+</head>
+<body>
+    <header class="site-header">
+      <nav class="navbar navbar-expand-md navbar-dark bg-steel fixed-top">
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-6">
-                    <div class="card text-dark bg-light shadow">
-                        <div class="card-body">
-                            <h1 class="card-title text-dark text-center">Contact Manager</h1>
-                            <a class="shadow btn btn-primary btn-block" href="login.php" roll="button">Go To War</a>
-                            <a class="shadow btn btn-primary btn-block" href="sign_up.php" roll="button">Enlist</a>
-                            <a class="shadow btn btn-primary btn-block" href="Deflate" roll="button">Deflate</a>
-                        </div>
-                    </div>
-                </div>
+          <a class="navbar-brand mr-4" href="/Deflate">Deflate Blog</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarToggle">
+            <div class="navbar-nav mr-auto">
+              <a class="nav-item nav-link" href="/Deflate">Home</a>
+              <a class="nav-item nav-link" href="/About">About</a>
             </div>
+            <!-- Navbar Right Side -->
+            <div class="navbar-nav">
+
+<!--Empty for now-->
+
+            </div>
+          </div>
         </div>
-    </div>
-   
-   
- </body>
- <footer>
-      <!--<center><div class ="open-button"><a href="http://getamped2.cyberstep.com/" target="_blank"><img src="shooting.gif"></a></div></center>-->
- </footer>
+      </nav>
+    </header>
+    <main role="main" class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <p>Thank you for coming to our site</p>
+          <p>Our mission is to provide basic knowledge on the most widely used compression algorithm, known as Deflate.</p>
+          <p>We are amatuer developers at the Univeristy of Central Florida</p>
+          <p>If there are any errors, please feel free to contact us at Richard.Leinecker@ucf.edu or at (407) 823-0169 or directly at our office at University of Central Florida Harris Engineering building, Room 357</p><br><br>
+          <p><strong>Our Team members:</strong></p>
+          <p>Graphics & UI: Dat Nguyen</p>
+          <p>Codes: Wyatt Jones</p>
+          <p>Writing/Explanation: Emily Thomas & Caleb Jones</p>
+         
+          <br>
+          <br>
+          <p>Special thanks to our advisor for guiding us alonng the way</p>
+          <p><strong>Our Advisor: </strong>Dr. Richard Leinecker</p>
+          
+        </div>
+      </div>
+    </main>
+
+
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
 </html>
